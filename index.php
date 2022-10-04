bb0fa1e1cdafe032e6e268d5723289b1fff94257
Michelle <michelle.alabi@gmail.com>
2022-10-04 11:26:44 +0200

    Structure du site

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require 'header.php';
+    $title= "Acueil";
+    $meta = "Présentation de Michelle Alabi";
+?>
+
+
+        <h1>Michelle Alabi</h1>
+        <p>Développeuse web</p>
+
+
+<?php
+require 'footer.php';
+?>
---
+++ loisirs.php
@@ -0,0 +1,15 @@
+<?php
+require 'header.php';
+$title= "Loisirs";
+$meta = "Compétences en programmation web";
+?>
+
+    <ul>
+        <li>Voyages</li>
+        <li>Randonnées</li>
+    </ul>
+
+<?php
+require 'footer.php';
+?>
+
