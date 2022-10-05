a85a15e5cd82a821906cd135347cbe90be3b5e4a
Michelle <michelle.alabi@gmail.com>
2022-10-05 23:59:10 +0200

    Modifications des erreurs de validation du formulaire

--- contact.php
+++ contact.php
@@ -13,16 +13,7 @@ $doc="";
 
 if(!empty($_POST))
 {
-    $_SESSION =
-        array(
-            'title' => htmlspecialchars($_POST['title'], ENT_QUOTES),
-            'firstname' => htmlspecialchars($_POST['firstname'], ENT_QUOTES),
-            'lastname' => htmlspecialchars($_POST['lastname'], ENT_QUOTES),
-            'email' => htmlspecialchars($_POST['email'], ENT_QUOTES),
-            'info' => htmlspecialchars($_POST['info'], ENT_QUOTES),
-            'message' => htmlspecialchars($_POST['message'], ENT_QUOTES, "UTF-8")
-        );
-
+    
     //Traitement du formulaire
 
     if(empty($_POST['firstname'])){
@@ -41,7 +32,7 @@ if(!empty($_POST))
         $msg = "Merci de remplir ce champ";
         $message='<div class="alert alert-danger mb-5">Merci de remplir les champs indiqués</div>';
     }
-    if(empty($_POST['info'] || $_POST['other_info'] )){
+    if(empty($_POST['info'])){
         $msg_info = "Merci de choisir le motif";
         $message='<div class="alert alert-danger mb-5">Merci de remplir les champs indiqués</div>';
     }
@@ -54,11 +45,19 @@ if(!empty($_POST))
     }
 
 }
-if(empty($message) && empty($content)){
-    $message = "<div>Formulaire validé</div>";
+if(empty($message) && empty($content) && !empty($_POST)){
+    $_SESSION =
+        array(
+            'title' => htmlspecialchars($_POST['title'], ENT_QUOTES),
+            'firstname' => htmlspecialchars($_POST['firstname'], ENT_QUOTES),
+            'lastname' => htmlspecialchars($_POST['lastname'], ENT_QUOTES),
+            'email' => htmlspecialchars($_POST['email'], ENT_QUOTES),
+            'info' => htmlspecialchars($_POST['info'], ENT_QUOTES),
+            'message' => htmlspecialchars($_POST['message'], ENT_QUOTES, "UTF-8"),
+        );
     $doc = implode(PHP_EOL,$_SESSION);
     file_put_contents('form.txt', $doc, FILE_APPEND);
-    unset($_SESSION);
+   
 }
 
 ?>
@@ -67,37 +66,37 @@ if(empty($message) && empty($content)){
 <form action="contact.php" method="post">
     <form>
         <select class="form-select" id = "title" aria-label="Default select example" name="title">
-            <option value="vide">Civilité</option>
+            <option value="">Civilité</option>
             <option value="mr" <?= (isset($_POST['title']) && $_POST['title'] == "mr") ? "selected" : ""?> >Monsieur</option>
             <option value="mme" <?= (isset($_POST['title']) && $_POST['title'] == "mme") ? "selected" : ""?> >Madame</option>
         </select>
-        <p class="small "><small class="text-danger"><?=(isset($_POST) && ($_POST['title'] == "vide")) ? $msg_title : "" ?></small></p>
+        <p class="small "><small class="text-danger"><?=(isset($_POST['title']) && (empty($_POST['title']))) ? $msg_title : "" ?></small></p>
         <div class="mb-3">
             <label for="lastname" class="form-label">Nom</label>
             <input type="text" class="form-control" id="lastname" name="lastname" value="<?= (isset($_POST['lastname'])) ? $_SESSION['lastname'] : "" ?>">
-            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['lastname'])) ? $msg : "" ?></small></p>
+            <p class="small "><small class="text-danger"><?=(isset($_POST['lastname']) && empty($_POST['lastname'])) ? $msg : "" ?></small></p>
         </div>
         <div class="mb-3">
             <label for="firstname" class="form-label">Prénom</label>
             <input type="text" class="form-control" id="firstname" name="firstname" value="<?=(isset($_POST['firstname']) ? $_SESSION['firstname'] : "")?>">
-            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['firstname'])) ? $msg : "" ?></small></p>
+            <p class="small "><small class="text-danger"><?=(isset($_POST['firstname']) && empty($_POST['firstname'])) ? $msg : "" ?></small></p>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" name="email" value="<?=(isset($_POST['email']) ? $_SESSION['email'] : "")?>" >
-            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['email'])) ? $msg : "" ?><?=(isset($_POST) && !empty($content)) ? $content : "" ?></small></p>
+            <p class="small "><small class="text-danger"><?=(isset($_POST['email']) && empty($_POST['email'])) ? $msg : "" ?><?=(isset($_POST) && !empty($content)) ? $content : "" ?></small></p>
         </div>
         <div>
             <div>Motif de contact</div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" value="Demande d'informations" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Demande d'informations") ? "checked" : ""?>>
-                <label class="form-check-label" for="info">
+                <input class="form-check-input" type="radio" value="Demande d'informations" id ="information" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Demande d'informations") ? "checked" : ""?>>
+                <label class="form-check-label" for="information">
                     Demande d'informations
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" value="Autres demandes" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Autres demandes") ? "checked" : ""?>>
-                <label class="form-check-label" for="info">
+                <input class="form-check-input" type="radio" value="Autres demandes" id="other_info" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Autres demandes") ? "checked" : ""?>>
+                <label class="form-check-label" for="other_info">
                     Autres demandes
                 </label>
             </div>
