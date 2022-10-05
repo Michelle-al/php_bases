e9b6ffa7b65561637f0c78fc41c62296bfc25839
michelle <michelle.alabi@gmail.com>
2022-10-05 14:33:24 +0200

    Validation du formulaire

--- contact.php
+++ contact.php
@@ -1,7 +1,9 @@
 <?php
-$title= "Formulmaire contact";
+$title= "Formulaire de contact";
 $meta = "Me contacter";
 require 'header.php';
+session_start();
+
 
 $message='';
 $msg_message ='';
@@ -9,6 +11,16 @@ $msg_info ='';
 
 if(!empty($_POST))
 {
+
+    $_SESSION =
+        array(
+            'title' => $_POST['title'],
+            'firstname' => $_POST['firstname'],
+            'lastname' => $_POST['lastname'],
+            'email' => $_POST['email'],
+            'info' => $_POST['info'],
+            'message' => $_POST['message']
+        );
 
     //Traitement du formulaire
 
@@ -35,59 +47,63 @@ if(!empty($_POST))
     if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) && (!empty($_POST['email']))){
         $content = "L'adresse email n'est pas valide";
     }
-    if(empty($_POST['message'] && strlen($_POST['message']))){
+    if(empty($_POST['message'] ) || strlen($_POST['message']) < 5){
         $msg_message = "Merci de remplir ce champ avec un mot de plus de 5 lettres";
         $message='<div class="alert alert-danger mb-5">Merci de remplir les champs indiqués</div>';
     }
 
 }
 if(empty($message) && empty($content)){
-    $message = "<div>Formulaire validé</div>";
+    $messages = "<div>Formulaire validé</div>";
 }
 
 ?>
+<h1 class="text-center mb-5">Me contacter</h1>
+
 <form action="contact.php" method="post">
     <form>
-        <select class="form-select" id = "title" aria-label="Default select example">
-            <option selected>Civilité</option>
-            <option value="mr">Monsieur</option>
-            <option value="mme">Madame</option>
-            <p class="small "><small class="text-danger"><?=(empty($_POST['title']) && !empty($_POST)) ? $msg : ''?></small></p>
+        <select class="form-select" id = "title" aria-label="Default select example" name="title">
+            <option value="vide">Civilité</option>
+            <option value="mr" <?= (isset($_POST['title']) && $_POST['title'] == "mr") ? "selected" : ""?> >Monsieur</option>
+            <option value="mme" <?= (isset($_POST['title']) && $_POST['title'] == "mme") ? "selected" : ""?> >Madame</option>
         </select>
+        <p class="small "><small class="text-danger"><?=(isset($_POST) && ($_POST['title'] == "vide")) ? $msg : "" ?></small></p>
         <div class="mb-3">
             <label for="lastname" class="form-label">Nom</label>
-            <input type="text" class="form-control" id="lastname" >
-            <p class="small "><small class="text-danger"><?=(empty($_POST['lastname']) && !empty($_POST)) ? $msg : ''?></small></p>
+            <input type="text" class="form-control" id="lastname" name="lastname" value="<?= (isset($_POST['lastname'])) ? $_SESSION['lastname'] : "" ?>">
+            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['lastname'])) ? $msg : "" ?></small></p>
         </div>
         <div class="mb-3">
             <label for="firstname" class="form-label">Prénom</label>
-            <input type="text" class="form-control" id="firstname" >
-            <p class="small "><small class="text-danger"><?=(empty($_POST['firstname']) && !empty($_POST)) ? $msg : ''?></small></p>
+            <input type="text" class="form-control" id="firstname" name="firstname" value="<?=(isset($_POST['firstname']) ? $_SESSION['firstname'] : "")?>">
+            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['firstname'])) ? $msg : "" ?></small></p>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" >
-            <p class="small "><small class="text-danger"><?=(empty($_POST['email']) && !empty($_POST)) ? $msg : ''?><?= (isset($content)) ? $content : ""?></small></p>
+            <input type="email" class="form-control" id="email" name="email" value="<?=(isset($_POST['email']) ? $_SESSION['email'] : "")?>" >
+            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['email'])) ? $msg : "" ?><?=(isset($_POST) && !empty($content)) ? $content : "" ?></small></p>
         </div>
-        <div>Motif de contact</div>
-        <p class="small "><small class="text-danger"><?=(empty($_POST['firstname']) && !empty($_POST)) ? $msg : ''?></small></p>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" value="" id="info" name=" info">
-            <label class="form-check-label" for="info">
-                Demande d'informations
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" value="" id="other_info" name="other_info">
-            <label class="form-check-label" for="other_info">
-                Autres demandes
-            </label>
+        <div>
+            <div>Motif de contact</div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="Demande d'informations" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Demande d'informations") ? "checked" : ""?>>
+                <label class="form-check-label" for="info">
+                    Demande d'informations
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="Autres demandes" name="info" <?= (isset($_POST['info']) && $_POST['info'] == "Autres demandes") ? "checked" : ""?>>
+                <label class="form-check-label" for="info">
+                    Autres demandes
+                </label>
+            </div>
+            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['info'])) ? $msg_info : "" ?></small></p>
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">Votre message</label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
-            <p class="small "><small class="text-danger"><?=(empty($_POST['firstname']) && !empty($_POST)) ? $msg : ''?></small></p>
+            <textarea class="form-control" id="message" name="message" rows="3"><?=(isset($_POST['message']) ? $_SESSION['message'] : "")?></textarea>
+            <p class="small "><small class="text-danger"><?=(isset($_POST) && empty($_POST['message']) || strlen($_POST['message']) < 5) ? $msg_message : "" ?></small></p>
         </div>
-        <input type="submit" class="btn btn-primary mb-3">
+        <input type="submit" class="btn btn-primary mb-3" >
     </form>
 </form>
